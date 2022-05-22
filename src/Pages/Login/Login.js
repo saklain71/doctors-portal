bc9b91e9ab5c239d16d150e30b1a709e10f61c4e
Saklain Mustak <saklainmustak@sakmac.local>
2022-05-22 15:33:25 +0600

    firest section designed

---
+++ src/Pages/Login/Login.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            {/* <div class="h-full w-full"
+                style="background-image: url('https://image.shutterstock.com/image-photo/mountains-under-mist-morning-amazing-260nw-1725825019.jpg');">
+                <div>y
+
+                </div> */}
+
+            
+        </div>
+    );
+};
+
+export default Login;
